92361a28241c4f92c465aeb5a2ec6956081e07ee
abdulrahmanbhatti <abdulrehmanbhatti0010@gmail.com>
2026-05-01 08:52:51 +0500

    admin

---
+++ admin/login.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+include('../include/connection.php');
+if(isset($_POST['login'])){
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $query = "SELECT * FROM admins WHERE email ='$email' AND password ='$password'";
+   $result= $conn->query($query);
+
+   if($result->num_rows > 0){
+    $admin =  $result->fetch_assoc();
+    $_SESSION['email']=$voter['email'];
+    $_SESSION['name']=$voter['name'];
+    $_SESSION['id']=$voter['vid'];
+    header('location:dashboard.php');
+
+   }
+   else {
+        echo "<script>
+        alert('Invalid Credentials ');
+         window.location.href = '../index.php';
+        </script>";
+       
+   }
+}
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voting system </title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <script src="../bootstrap/js/bootstrap.min.js "></script>
+</head>
+<body>
+    <div class="container-fluid header ">
+        <h3>Online Voting System </h3>
+    </div>
+   <div class="container-fluid mt-5">
+    <div class="row">
+        <div class="col-md-3 m-auto voter-login-form">
+            <center><h4><u>Admin Login Form </u></h4></center>
+            <br>
+            <form action="login.php" method = "POST">
+             <div class="form-group">
+                <label for="email">Enter Email </label>
+                <input type="email" class ="form-control" name="email" placeholder="Enter email " required >
+            </div>
+             <div class="form-group">
+                <label for="password">Your password  </label>
+                <input type="password" class ="form-control" name="password" placeholder="Your password " required >
+            </div>
+            <button type="submit" class="btn btn-primary" name="login">login</button>
+            <!-- <input type="submit" class="btn btn-primary" value="Register"
+            name="register"> -->
+          
+            </form>
+        </div>
+    </div>
+   </div>
+</body>
+</html>
--- voters/edit_profile.php
+++ voters/edit_profile.php
@@ -1,3 +1,43 @@
+<?php
+session_start();
+include('../include/connection.php');
+if(isset($_POST['Update'])){
+    $name= $_POST['name'];
+    $email= $_POST['email'];
+    $mobile= $_POST['mobile'];
+    $address= $_POST['address'];
+    // $query="UPDATE voters SET name= '$name',email='$email',mobile='$mobile',address='$address' WHERE vid=$_SESSION[vid]";
+    $query="UPDATE voters SET name= '$name',email='$email',mobile='$mobile',address='$address' WHERE vid=$_SESSION[vid]";
+   $result= $conn->query($query);
+   if($result){
+     echo "<script >
+        alert('voter Updated  Successfully');
+        window.location.href = 'dashboard.php';
+        </script>";
+
+   }
+   else{
+      echo "<script >
+        alert('Error plz try again ');
+        window.location.href = 'dashboard.php';
+        </script>";
+
+   }
+
+}
+$query = "SELECT * FROM voters WHERE vid ='$_SESSION[vid]'";
+$result= $conn->query($query);
+$voter=$result->fetch_assoc();
+if(isset($_POST['Update']))
+
+
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,28 +56,29 @@
     <div class="row">
         <div class="col-md-4 mt-4 m-auto" id="edit_profile">
             <center><h4><u>Edit Profile</u></h4></center>
-            <form action="register.php" method="POST"  onsubmit =" return validateForm()">
+            <form action="" method="POST"  onsubmit =" return validateForm()">
             <div class="form-group">
                 <label for="name">Enter Name </label>
-                <input type="text" class ="form-control" name="name" id="name"placeholder="Enter Name " required >
+                <input type="text" class ="form-control" name="name" id="name" value = "<?php echo $voter['name']?>" required >
                 <small id ="nameError" class="form-text text-danger"></small>
             </div>
              <div class="form-group">
                 <label for="email">Enter Email </label>
-                <input type="email" class ="form-control" name="email" id="email" placeholder="Enter email " required >
+                <input type="email" class ="form-control" name="email" id="email" value = "<?php echo $voter['email']?>" required >
                  <small id ="emailError" class="form-text text-danger"></small>
             </div>
              <div class="form-group">
                 <label for="name">Mobile Number  </label>
-                <input type="text" class ="form-control" name="mobile" id="mobile" placeholder="mobile Number  " required >
+                <input type="text" class ="form-control" name="mobile" id="mobile" value = "<?php echo $voter['mobile']?>"required >
                  <small id ="mobileError" class="form-text text-danger"></small>
             </div>
             <div class="form-group">
                 <label for="name"> your address </label>
-                <textarea name="address" class="form-control" row="2" col="50"></textarea>
+                <textarea name="address" class="form-control" row="2" col="50">value = "<?php echo $voter['address']?>"</textarea>
             </div>
             <br>
-            <button type="submit" class="btn btn-primary" name="register">Register</button>
+            <button type="submit" class="btn btn-primary" name="Update">Update</button>
+            <a href="dashboard.php" class="btn btn-danger mt-1">Go to Dashboard</a>
             <!-- <input type="submit" class="btn btn-primary" value="Register"
             name="register"> -->
             </form>
